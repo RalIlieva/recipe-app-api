ca072f2a4690adf566f300b3de59c51581839a7f
RalIlieva <rilieva8820@gmail.com>
2024-06-05 17:41:50 +0300

    Add the normalization functionality in core/models.py - use the normalization method from the BaseUserManager

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_field):
         """Create, save and return a new user"""
-        user = self.model(email=email, **extra_field)
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
 
