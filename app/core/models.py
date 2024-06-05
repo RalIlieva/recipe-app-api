7d68e77d534ada55f4761212f67d0ab8005a2e3e
RalIlieva <rilieva8820@gmail.com>
2024-06-05 17:26:57 +0300

    Implement user model - makemigrations, migrate - but needed to clear the db so the migrations are successful.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,34 @@
-# from django.db import models
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user"""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
