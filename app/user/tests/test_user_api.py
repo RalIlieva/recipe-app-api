e08e46045da798ea0c95500a6cef448879d3d33f
RalIlieva <rilieva8820@gmail.com>
2024-06-11 23:26:20 +0300

    Add test create a token with blank password

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -86,3 +86,10 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test posting a blank password return an error."""
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
