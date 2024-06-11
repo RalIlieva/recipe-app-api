e6a0473288be15ec0ccf7d53f8c8721151e72689
RalIlieva <rilieva8820@gmail.com>
2024-06-11 22:54:53 +0300

    Implement create user API - app/user/serializer.py, app/user/views.py, app/user/urls.py and map to app/app/ursl.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +26,6 @@ urlpatterns = [
     path('api/docs/',
          SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs',
-         )
+         ),
+    path('api/user/', include('user.urls')),
 ]
