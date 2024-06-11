295cd0c744131d867df61944c3c5a84802eb62ca
RalIlieva <rilieva8820@gmail.com>
2024-06-11 23:20:51 +0300

    Add test create token bad equest

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -78,3 +78,11 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, payload)
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials invalid."""
+        create_user(email='test@example.com', password='goodpass')
+        payload = {'email': 'test@examle.com', 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
